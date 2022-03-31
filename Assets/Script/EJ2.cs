e34b4b1f9ebecd5044cdd19f6182e524dd7ff5e9
FaustoCitera <101577508+FaustoCitera@users.noreply.github.com>
2022-03-31 08:30:39 -0300

    exercise 1 ended

---
+++ Assets/Script/EJ2.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EJ2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+		//2. Realizá un programa que, siendo num1 y num2 variables enteras con valores 4 y 5,
+	//respectivamente, realice la operación correspondiente y muestre el resultado en pantalla: a.Suma b.Producto
+	{
+		int num1;
+		int num2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
