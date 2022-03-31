1224addecfbd516c96acb98b046d7e587ce71574
FaustoCitera <101577508+FaustoCitera@users.noreply.github.com>
2022-03-31 08:34:13 -0300

    exercise 2 ended

--- Assets/Script/EJ2.cs
+++ Assets/Script/EJ2.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class EJ2 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+	public int num1 = 4;
+	public int num2 = 5;
+	// Start is called before the first frame update
+	void Start()
 		//2. Realizá un programa que, siendo num1 y num2 variables enteras con valores 4 y 5,
 	//respectivamente, realice la operación correspondiente y muestre el resultado en pantalla: a.Suma b.Producto
 	{
-		int num1;
-		int num2;
+		int suma = num1 + num2;
+		int producto = num1 * num2;
+		Debug.Log("suma= " + suma + "Producto= " + producto);
     }
 
     // Update is called once per frame
